54b8ecb86cce02f3604705dffda535297ced54ca
stanislavBozhanov <stanislav.bozhanov@gmail.com>
2014-01-02 14:34:45 +0200

    BubbleSort

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+class BubbleSort
+{
+    static void Main()
+    {
+        int[] array = { 1, 7, 3, 22, 32, 35, 1, 3, 1, 46, 78, 3, 3, 45, };
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            bool swapped = false;
+            for (int j = array.Length - 1; j >= 1 + i ; j--)
+            {
+                if (array[j] < array[j - 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+            {
+                break;
+            }
+        }
+    }
+}
