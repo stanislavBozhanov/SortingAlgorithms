703cdd9696364ee843414a7dd12c5bc713f7d43e
stanislavBozhanov <stanislav.bozhanov@gmail.com>
2014-01-03 15:54:22 +0200

    InsertionSort

--- BubbleSort/BubbleSort.cs
+++ BubbleSort/BubbleSort.cs
@@ -7,10 +7,10 @@ class BubbleSort
     {
         int[] array = { 1, 7, 3, 22, 32, 35, 1, 3, 1, 46, 78, 3, 3, 45, };
 
-        for (int i = 0; i < array.Length - 1; i++)
+        for (int i = 0; i < array.Length; i++)
         {
             bool swapped = false;
-            for (int j = array.Length - 1; j >= 1 + i ; j--)
+            for (int j = array.Length - 1; j > 1 + i ; j--)
             {
                 if (array[j] < array[j - 1])
                 {
--- InsertionSort/InsertionSort.cs
+++ InsertionSort/InsertionSort.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace InsertionSort
+class InsertionSort
 {
-    class InsertionSort
+    static void Main()
     {
-        static void Main(string[] args)
+        int[] array = { 1, 7, 3, 22, 32, 35, 1, 3, 1, 46, 78, 3, 3, 45, };
+
+        for (int i = 1; i < array.Length; i++)
         {
+            for (int k = i; k > 0 && array[k] < array[k - 1]; k--)
+            {
+                int temp = array[k - 1];
+                array[k - 1] = array[k];
+                array[k] = temp;
+            }
         }
     }
 }
